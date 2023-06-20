a2fd2d67a3fe6d4eda92ec17c94436488756080f
john <chukwuchidieberejohn@gmail.com>
2023-06-20 10:41:09 +0100

    Feat: cloudinary for image upload

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import L from "leaflet";
 import {MapContainer, Marker, TileLayer} from "react-leaflet";
 
+// Load the Leaflet marker icons
 import "leaflet/dist/leaflet.css";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 // @ts-ignore
+// Fetch the asset URLs
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
     iconUrl: markerIcon.src,
