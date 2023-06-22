5b20a4b4e5dde96f5085d964e7c9af1a37428a5b
john <chukwuchidieberejohn@gmail.com>
2023-06-22 15:10:52 +0100

    Feat(update): individual property listing and property reservation placement

--- app/components/Listings/ListingReservation.tsx
+++ app/components/Listings/ListingReservation.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React from "react";
 import {Range} from "react-date-range";
+import Calendar from "@/app/components/Inputs/Calendar";
+import Button from "@/app/components/Button";
 
 interface IListingReservationProps {
     price: number;
@@ -22,8 +24,28 @@ const ListingReservation: React.FC<IListingReservationProps> = ({
                                                                     disabledDate
                                                                 }) => {
     return (
-        <div>
-            ListingReservation
+        <div className="bg-white rounded-xl border-neutral-200">
+            <div className="flex items-center flex-row gap-1 p-4">
+                <div className="font-semibold text-2xl">{price}</div>
+                <div>night</div>
+            </div>
+            <hr/>
+            <Calendar
+                value={dateRange}
+                disabledDates={disabledDate}
+                onChange={(value) => onChangeDate(value.selection)}
+            />
+            <hr/>
+
+            <div className="flex flex-row items-center p-4">
+                <div>Total Price</div>
+                <div>{totalPrice}</div>
+            </div>
+            <hr/>
+
+            <div className="py-4">
+                <Button label="Make your reservation" onClick={onSubmit} disabled={disabled}/>
+            </div>
         </div>
     );
 };
